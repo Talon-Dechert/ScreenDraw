d1bf3bf7d8e0ff3526f1b196b908dffe04756bd7
Talon Dechert <techdechert@outlook.com>
2022-01-26 16:40:20 -0800

    Finish functionality of generateSquares function

--- script.js
+++ script.js
@@ -19,17 +19,22 @@ function generateSquares(numOfSquares){
 
     let sketchPad = document.querySelector('.sketchPad');
     
-    let height = `${(determineSquareSize(numOfSquares) - 1)}px`;
-    let width = `${(determineSquareSize(numOfSquares)- 1)}px`;
+    let height = `${(determineSquareSize(numOfSquares) - 2)}px`;
+    let width = `${(determineSquareSize(numOfSquares)- 2)}px`;
+
+    // let squareArray = [];
     
-    for (i = numOfSquares; i <= numOfSquares; i++){
-        for (i = numOfSquares; i <= numOfSquares; i++){
+    for (y = 1; y <= numOfSquares; y++){
+        for (x = 1; x <= numOfSquares; x++){
             let colorSquare = document.createElement("div");
+            colorSquare.setAttribute('id', `square${y}.${x}`)
             colorSquare.classList.add('colorSquare');
             colorSquare.setAttribute('style', `height: ${height}; width: ${width}`);
             sketchPad.appendChild(colorSquare);
         }
     }
+
+    return;
 }
 
 //Logic for square size
@@ -49,4 +54,6 @@ function sketchGame(numOfSquares){
     generateSquares(numOfSquares);
 }
 
-module.exports = determineSquareSize;
+// generateSquares(100);
+
+// module.exports = determineSquareSize;
